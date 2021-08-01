53194b322cbb76e39a3add592686e423cd9c1107
Tan Kui An Andrew <kui-an-andrew.tan@vie.com.my>
2021-08-01 23:54:09 +0800

    standardized documentationComponent router div

--- src/app/main-contents/documentation/documentation-routing.module.ts
+++ src/app/main-contents/documentation/documentation-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DocumentationComponent } from './documentation.component';
 import { GlobalStyleComponent } from './global-style/global-style.component';
+import { ScssComponent } from './scss/scss.component';
 
 const routes: Routes = [
   {
     path: '',
     component: DocumentationComponent,
     children: [
+      {
+        path: 'scss',
+        component: ScssComponent
+      },
       {
         path: 'globalStyle',
         component: GlobalStyleComponent
